e28bd379a5c85b9173c2a89cb778fdce259ef89e
Ubaidahmed221 <101449593+Ubaidahmed221@users.noreply.github.com>
2025-04-27 12:35:48 +0500

    navbar complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
-            
-        </div>
+        <section className='home-container grid' >
+
+        </section>
     )
 }
 
