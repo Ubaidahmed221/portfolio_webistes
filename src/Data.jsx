4f6e8afc528cb6e7cd9dcb380473686b6093cf8c
Ubaidahmed221 <101449593+Ubaidahmed221@users.noreply.github.com>
2025-04-27 15:45:19 +0500

    About Page Complete

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,4 +1,17 @@
 import { FaHome, FaUser, FaFolderOpen, FaEnvelopeOpen } from 'react-icons/fa';
+import skillsImg1 from './assets/html-logo.svg';
+import skillsImg2 from './assets/css-logo.svg';
+import skillsImg3 from './assets/javascript-logo.svg';
+import skillsImg4 from './assets/react-logo.svg';
+import skillsImg5 from './assets/tailwind-css-logo.svg';
+
+import skillsImg6 from './assets/figma-logo.svg';
+import skillsImg7 from './assets/sketch-logo.svg';
+import skillsImg8 from './assets/adobe-xd-logo.svg';
+import skillsImg9 from './assets/adobe-photoshop-logo.svg';
+import skillsImg10 from './assets/invision-logo.svg';
+
+import { RiBriefcase4Fill, RiGraduationCapFill } from 'react-icons/ri';
 
 export const links = [
     {
@@ -25,3 +38,218 @@ export const links = [
       path: '/contact',
     },
   ];
+
+  
+// ========== INFO ==========
+export const personalInfo = [
+  {
+    title: 'First Name : ',
+    description: 'Ubaid',
+  },
+
+  {
+    title: 'Last Name : ',
+    description: 'Ahmed',
+  },
+
+  {
+    title: 'Age : ',
+    description: '22 Years',
+  },
+
+  {
+    title: 'Nationality : ',
+    description: 'Pakistan',
+  },
+
+  {
+    title: 'Freelance : ',
+    description: 'Available',
+  },
+
+  {
+    title: 'Address : ',
+    description: 'Pakistan',
+  },
+
+  {
+    title: 'Phone : ',
+    description: '+92 3120003019',
+  },
+
+  {
+    title: 'Email : ',
+    description: 'ubaidahmed1282@gmail.com',
+  },
+
+  {
+    title: 'Skype : ',
+    description: 'ubaid ahmed',
+  },
+
+  {
+    title: 'Langages : ',
+    description: 'Urdu, English',
+  },
+];
+
+export const stats = [
+  {
+    no: '4+',
+    title: 'Years of <br /> Experience',
+  },
+
+  {
+    no: '10+',
+    title: 'Completed <br /> Projects',
+  },
+
+  {
+    no: '15+',
+    title: 'Happy <br /> Customers',
+  },
+
+  {
+    no: '10+',
+    title: ' Awards <br /> Won',
+  },
+];
+
+export const skill = [
+  {
+    id: 1,
+    img: skillsImg1,
+    title: 'HTML',
+    level: 'Intermediate',
+    category: 'developer',
+  },
+
+  {
+    id: 2,
+    img: skillsImg2,
+    title: 'CSS',
+    level: 'Advanced',
+    category: 'developer',
+  },
+
+  {
+    id: 3,
+    img: skillsImg3,
+    title: 'JavaScript',
+    level: 'Intermediate',
+    category: 'developer',
+  },
+
+  {
+    id: 4,
+    img: skillsImg4,
+    title: 'React',
+    level: 'Basic',
+    category: 'developer',
+  },
+
+  {
+    id: 5,
+    img: skillsImg5,
+    title: 'Tailwind',
+    level: 'Intermediate',
+    category: 'developer',
+  },
+
+  {
+    id: 6,
+    img: skillsImg6,
+    title: 'Figma',
+    level: 'Advanced',
+    category: 'designer',
+  },
+
+  {
+    id: 7,
+    img: skillsImg7,
+    title: 'Sketch',
+    level: 'Basic',
+    category: 'designer',
+  },
+
+  {
+    id: 8,
+    img: skillsImg8,
+    title: 'Adobe XD',
+    level: 'Intermediate',
+    category: 'designer',
+  },
+
+  {
+    id: 9,
+    img: skillsImg9,
+    title: 'Photoshop',
+    level: 'Intermediate',
+    category: 'designer',
+  },
+
+  {
+    id: 10,
+    img: skillsImg10,
+    title: 'InvisionApp',
+    level: 'Basic',
+    category: 'designer',
+  },
+];
+
+
+export const resume = [
+  {
+    id: 1,
+    category: 'experience',
+    icon: <RiBriefcase4Fill />,
+    year: '2018 - Present',
+    title: 'Web Developer <span> Envato </span>',
+    desc: 'Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit.',
+  },
+
+  {
+    id: 2,
+    category: 'experience',
+    icon: <RiBriefcase4Fill />,
+    year: '2013 - 2018',
+    title: 'UI/UX Designer <span> Themeforest </span>',
+    desc: 'Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol elit, tempor incididunt.',
+  },
+
+  {
+    id: 3,
+    category: 'experience',
+    icon: <RiBriefcase4Fill />,
+    year: '2005 - 2013',
+    title: 'Consultant <span> Videohive </span>',
+    desc: 'Lorem ipsum dolor sit amet, tempor incididunt ut laboreconsectetur elit, sed do eiusmod tempor duntt.',
+  },
+
+  {
+    id: 4,
+    category: 'education',
+    icon: <RiGraduationCapFill />,
+    year: '2015',
+    title: 'Engineering Degree <span> Oxford University </span>',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do tempor incididunt ut labore.',
+  },
+
+  {
+    id: 5,
+    category: 'education',
+    icon: <RiGraduationCapFill />,
+    year: '2012',
+    title: 'Master Degree <span> KIEV University </span>',
+    desc: 'Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol elit, tempor incididunt.',
+  },
+
+  {
+    id: 6,
+    category: 'education',
+    icon: <RiGraduationCapFill />,
+    year: '2009',
+    title: 'Bachelor Degree <span> Tunis High School </span>',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore.',
+  },
+];
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,96 @@
 import React from 'react'
+import Info from "../../components/info";
+import Cv from '../../pdf/steve-cv.pdf';
+import { RiDownload2Line } from "react-icons/ri";
+import Stats from '../../components/stats';
+import { skill } from '../../Data';
+import { resume } from '../../Data';
+import ResumeItem from '../../components/resumeItem';
+import SkillsItem from '../../components/skillsItem';
+import './about.css';
 
 function About() {
     return (
-        <div>
-            
+       <main className="section container">
+        <section className='about'>
+            <h2 className="section-title">
+                About <span>Me</span>
+            </h2>
+
+            <div className="about-container grid">
+                <div className="about-info">
+                    <h3 className='section-subtitle' >Persnal Infos</h3>
+
+                    <ul className="info-list grid"> <Info /> </ul>
+
+                    <a href={Cv} download='' className="button">Download Cv
+
+                        <span className="button-icon"><RiDownload2Line /></span>
+                    </a>
+
+                </div>
+
+                <div className="stats grid">
+                    <Stats />
+                </div>
+            </div>
+
+        </section>
+
+        <div className="separator"></div>
+
+        <section className="skills">
+            <h3 className="section-subtitle subtitle-center">My Skills</h3>
+
+            <div className="skills-container grid">
+             <div className="skills-group">
+                <h3 className="skills-title">Frontend Developer</h3>
+
+                <div className="skills-items grid">
+                    {skill.map((val) => {
+                        if(val.category === 'developer'){
+                            return <SkillsItem key={val.id} {...val} />
+                        }
+                    })}
+                </div>
+             </div>
+             <div className="skills-group">
+                <h3 className="skills-title">Web Designer</h3>
+
+                <div className="skills-items grid">
+                {skill.map((val) => {
+                        if(val.category === 'designer'){
+                            return <SkillsItem key={val.id} {...val} />
+                        }
+                    })}
+                </div>
+             </div>
+            </div>
+        </section>
+
+        <div className="separator"></div>
+
+        <section className="resume">
+        <h3 className="section-subtitle subtitle-center">Experience & Education</h3>
+        <div className="resume-container grid">
+                    <div className="resume-group grid">
+                        {resume.map((val) => {
+                            if(val.category === 'experience'){
+                                return <ResumeItem key={val.id} {...val} />;
+                            }
+                        })}
+                    </div>
+                    <div className="resume-group grid">
+                        {resume.map((val) => {
+                            if(val.category === 'education'){
+                                return <ResumeItem key={val.id} {...val} />;
+                            }
+                        })}
+                    </div>
         </div>
+        </section>
+
+       </main>
     )
 }
 
