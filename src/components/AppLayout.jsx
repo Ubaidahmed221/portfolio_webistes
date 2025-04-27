a1b349d65b7c4965f462601c092908bcf0cbd14c
Ubaidahmed221 <101449593+Ubaidahmed221@users.noreply.github.com>
2025-04-27 13:22:31 +0500

    Home Page Complete

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -8,7 +8,7 @@ function AppLayout() {
         <>
         <Navbar />
         <Outlet />
-        <Themes />
+        {/* <Themes /> */}
         </>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Profile from '../../assets/home.jpg';
+import {Link } from 'react-router';
+import { RiArrowRightLine } from "react-icons/ri";
+import './home.css';
+
 
 function Home() {
     return (
         <section className='home-container grid' >
+            <img src={Profile} alt="" className="home-img" />
+
+            <div className="home-content">
+                <h1 className="home-title"><span> I'm Ubaid Ahmed </span> <br /> Web Developer</h1>
+
+                <p className="home-decription">
+                I am a Tunisian based web designer & front-end developer focused on 
+                crafting clean & user-friendly experiences, I am passionate about building 
+                excellent software that improves the lives of those around me.
+                </p>
+
+                <Link to={'/about'} className='button' >More About Me
+                <span className="button-icon">
+                <RiArrowRightLine />
+                </span>
+                </Link>
+            </div>
+
+            <div className="color-block"></div>
 
         </section>
     )
